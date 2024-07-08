3dcd18d620dd11f1ccc4dbd5ed829a050512d8f9
Ian Hadrien <ianhadrien@gmail.com>
2024-07-08 19:21:01 -0300

    feat: add center pointer

--- src/repositories/prisma/prisma-plantations-repositories.ts
+++ src/repositories/prisma/prisma-plantations-repositories.ts
@@ -3,17 +3,48 @@ import { prisma } from '@/lib/prisma'
 import { PlantationsRepository } from '../interfaces/plantation-repository'
 import { PaginationType } from '@/@types/paginate'
 
+interface MapLatLng {
+  lat: string
+  lng: string
+}
+
+interface MapData {
+  lat: number
+  lng: number
+}
+
+interface FindMapDataResult {
+  mapFormatted: MapData[]
+  centerPoint: MapData
+}
+
 export class PrismaPlantationsRepository implements PlantationsRepository {
-  private async findMapDataByMappedAreaId (mappedAreaId: string): Promise<Array<{ lat: number, lng: number }>> {
-    const maps = await prisma.mapLatLng.findMany({
+  private async findMapDataByMappedAreaId (mappedAreaId: string): Promise<FindMapDataResult> {
+    const maps: MapLatLng[] = await prisma.mapLatLng.findMany({
       where: { mappedAreaId },
       orderBy: { position: 'asc' }
     })
 
-    return maps.map(map => ({
+    const mapFormatted: MapData[] = maps.map(map => ({
       lat: parseFloat(map.lat),
       lng: parseFloat(map.lng)
     }))
+
+    let sumLat = 0
+    let sumLng = 0
+    const count = mapFormatted.length
+
+    mapFormatted.forEach(coord => {
+      sumLat += coord.lat
+      sumLng += coord.lng
+    })
+
+    const centerPoint: MapData = {
+      lat: sumLat / count,
+      lng: sumLng / count
+    }
+
+    return { mapFormatted, centerPoint }
   }
 
   async getAll (page: number, pageSize: number): Promise<PaginationType<Plantation>> {
@@ -35,10 +66,12 @@ export class PrismaPlantationsRepository implements PlantationsRepository {
 
     const dataWithMapData = await Promise.all(
       data.map(async (plantation) => {
-        const mapData = await this.findMapDataByMappedAreaId(plantation.mappedAreaId)
-        return { ...plantation, mapData }
+        const { mapFormatted, centerPoint } = await this.findMapDataByMappedAreaId(plantation.mappedAreaId)
+        return { ...plantation, mapFormatted, centerPoint }
       })
     )
+
+    console.log(dataWithMapData)
 
     return { data: dataWithMapData, totalCount }
   }
